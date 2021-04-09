c0252c7af2872b53dcb657ec499fac5a334ad100
Steve Jones <steve.jones@red-gate.com>
2021-04-09 17:03:01 -0600

    dgfdg

---
+++ 02-refine_the_script.ps1
@@ -0,0 +1,8 @@
+$ServerUrl = 'http://Aristotle:14145' # Set to your Clone server URL
+$MachineName = 'Aristotle' # The machine name of the SQL Server instance to create the clones on
+$InstanceName = '' # The instance name of the SQL Server instance to create the clones on
+$ImageLocation = '\\Aristotle\SQLCloneImages' # Point to the file share we want to use to store the image
+$MaskingSetPath = 'E:\Documents\Data Masker(SqlServer)\Masking Sets' # The path to a masking set
+$DatabaseName = 'DMDemo_5_Prod' # The name of the database
+
+
